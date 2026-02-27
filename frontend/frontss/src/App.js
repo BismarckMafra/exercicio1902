f1fc01874e6c67b67877abb41b2e7fe8dc4a4809
Bismarck <bismarck_mafra@estudante.sesisenai.org.br>
2026-02-26 21:20:52 -0300

    v5

--- frontend/frontss/src/App.js
+++ frontend/frontss/src/App.js
@@ -8,6 +8,10 @@ import Header from './components/header/Header';
 import MainSection from './components/section/MainSection';
 import Footer from './components/footer/Footer';
 import Nav from './components/nav/MainNav';
+import SectionUser from './pages/SectionUser';
+import SectionCadastra from './pages/SectionCadastra';
+import SectionAtualiza from './pages/SectionAtualiza';
+import SectionDeleta from './pages/SectionDeleta';
 
 
 function App() {
